dc2e7ddaf791670e5315345a35d97e0363657cec
Fremount <3056847915@qq.com>
2025-08-22 14:18:50 +0800

    style: fix ESLint code style issues in management pages and utilities
    - Fix array bracket spacing in Manage components
    - Clean up code formatting in AgentSetting and AutoInstall
    - Fix style issues in Layout components
    - Improve code consistency in utility functions
    - Fix formatting in request interceptors
    
    These are automatic ESLint fixes for better code consistency across the application.

--- src/pages/Manage/AutoInstall/index.tsx
+++ src/pages/Manage/AutoInstall/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC, memo, useEffect, useState } from 'react';
 import Translation from 'components/Translation';
-import _ from 'lodash';
+import * as _ from 'lodash';
 import i18n from '../../../i18n';
 import locale from 'utils/locale';
 import styles from './index.css';
