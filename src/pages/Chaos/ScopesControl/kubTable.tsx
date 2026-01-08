caea72c07007502d63e1cdf638b52d6f1b78d8fd
xcaspar <changjun.xcj@alibaba-inc.com>
2026-01-08 17:11:50 +0800

    chore: update .gitignore and fix type assertion in kubTable
    - Added 'dist/' and 'dist.tgz' to .gitignore to prevent unnecessary files from being tracked.
    - Fixed type assertion in the expandedRowRender function of kubTable to improve code clarity.
    
    Signed-off-by: xcaspar <changjun.xcj@alibaba-inc.com>

--- src/pages/Chaos/ScopesControl/kubTable.tsx
+++ src/pages/Chaos/ScopesControl/kubTable.tsx
@@ -79,7 +79,7 @@ const KubTable: FC<IPorps> = props => {
     }
   }
   function expandedRowRender(record: any): JSX.Element {
-    return <K8sRowRender data={record} getData={getData}></K8sRowRender>;
+    return React.createElement(K8sRowRender as any, { data: record, getData }) as any;
   }
   const renderAgentVersion: any = (value: boolean) => {
     if (value) {
